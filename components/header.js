2c94c9c82b21ebd20110e701b520723281c47ea7
Chihyun Oh <chihyun621@kakao.com>
2022-11-11 00:55:30 +0900

    first commit

---
+++ components/header.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import DarkModeToggleButton from "./dark-mode-toggle-button";
+
+export default function Header(){
+    return(
+        <>
+            <header className="text-gray-600 body-font">
+                <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+
+                    <Link href="/">
+                        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+                            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+                        </svg>
+                        <span className="ml-3 text-xl">개발자 오치현</span>
+                        </a>
+                    </Link>
+
+                    <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
+                        <Link href="/">
+                            <a className="mr-5 hover:text-gray-900">홈</a>
+                        </Link>
+
+                        <Link href="/projects">
+                            <a className="mr-5 hover:text-gray-900">프로젝트</a>
+                        </Link>
+
+                        <a href="https://namecard.kakao.com/chihyun621" className="mr-5 hover:text-gray-900">연락하기</a>
+                    </nav>
+                    {/* 다크모드 토글 버튼 작업 해야함 */}
+                    <DarkModeToggleButton/>
+                </div>
+            </header>
+        </>
+    );
+}
